37a8aa72d428d64fe173d42460815dc25a59e4d3
endakenneally <endakenneally2@gmail.com>
2024-11-26 23:07:17 +0000

    Function added to save cleaned data to cleaned table

--- dags/clean_stag_batch.py
+++ dags/clean_stag_batch.py
@@ -18,14 +18,16 @@ dag = DAG(
     schedule_interval=None,
 )
 
+# Load config file
+dag_id = 'clean_stag_batch'
+with open(f'/opt/airflow/configs/{dag_id}_config.json') as config_file:
+    config = json.load(config_file)
+
 
 def get_data(batch_size, offset):
     # Here we will download the data in batches and then clean it
-    dag_id = 'clean_stag_batch'
-    with open(f'/opt/airflow/configs/{dag_id}_config.json') as config_file:
-        config = json.load(config_file)
 
-    url = f'{config['api_path']}{config['endpoint']}?limit={batch_size}&offset={offset}'
+    url = f'{config['api_path']}{config['get_endpoint']}?limit={batch_size}&offset={offset}'
     response = requests.get(url)
 
     data = response.json()
@@ -35,11 +37,15 @@ def get_data(batch_size, offset):
 
 
 def clean_data(batch_json):
+    df = pd.read_json(batch_json)
     # This function is used to clean the data using pandas
-    return
+
+    return df.to_json()
 
 
 def save_to_new_table(cleaned_json):
-    # Saves Data to new cleaned table
-
+    # Saves data to new cleaned table by calling save data API
+    url = f'{config['api_path']}{config['get_endpoint']}'
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=cleaned_json, headers=headers)
     return
