1fa21201be6b7f1b44ced5871ba45cac9d3bcce4
endakenneally <endakenneally2@gmail.com>
2024-11-29 23:48:32 +0000

    Cleaned up DAG and changed batch size in config

--- dags/clean_stag_batch.py
+++ dags/clean_stag_batch.py
@@ -2,8 +2,6 @@ from airflow import DAG
 import json
 import requests
 import pandas as pd
-from airflow.hooks.base_hook import BaseHook
-from sqlalchemy import create_engine, text
 from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 from airflow.models.xcom_arg import XComArg
@@ -31,9 +29,10 @@ with DAG(
     schedule_interval=None,
 ) as dag:
 
-    def get_data(batch_size, offset):
+    @task
+    def get_data(batch_task):
+        batch_size, offset = batch_task
         # Here we will download the data in batches and then clean it
-
         url = f"{config['api_path']}{config['get_endpoint']}?limit={batch_size}&offset={offset}"
 
         response = requests.get(url)
@@ -42,6 +41,7 @@ with DAG(
         # return to next task in the sequence
         return data
 
+    @task
     def clean_data(batch_json):
         # This function is used to clean the data using pandas
         df = pd.read_json(batch_json)
@@ -51,6 +51,7 @@ with DAG(
             lambda x: x[-5:].strip().upper())
         return df.to_json()
 
+    @task
     def save_to_new_table(cleaned_json):
         # Saves data to new cleaned table by calling save data API
         url = f"{config['api_path']}{config['save_endpoint']}"
@@ -62,7 +63,7 @@ with DAG(
     @task
     def create_batches(batch_size):
         # Get count of rows from the API
-        url = f"{config['api_dath']}{config['count_endpoint']}"
+        url = f"{config['api_path']}{config['count_endpoint']}"
         response = requests.get(url)
         count = response.json()['total_records']
         logging.info(f'Count {count}')
@@ -76,41 +77,8 @@ with DAG(
 
         return batch_tasks
 
-    # Task Flow
-    # First generate batches
-    generate_batches = PythonOperator(
-        task_id='generate_batches',
-        python_callable=create_batches,
-        op_args=[config['batch_size']],
-        do_xcom_push=True,
-    )
-
-    # Runs in order
-    def process_batch(batch_task):
-        batch_size, offset = batch_task
-        # get data
-        json = get_data(batch_size, offset)
-        # clean batch
-        cleaned_json = clean_data(json)
-        # save batch
-        save_to_new_table(cleaned_json)
-
-    # Create individual tasks for procesing
-    with TaskGroup("batch_processing", dag=dag) as batch_processing_group:
-        # batch_tasks = "{{ task_instance.xcom_pull(task_ids='generate_batches') }}"
-        batch_tasks = generate_batches.output
-        logging.info(f'{batch_tasks}')
-
-        for batch_task in batch_tasks:
-            # For every batch in batch_task
-            batch_process = PythonOperator(
-                # Unique task ID per batch - will be the offset value from batch_task tuple
-                task_id=f"process_batch_{batch_task[1]}",
-                python_callable=process_batch,
-                op_args=[batch_task],
-            )
-
-            generate_batches >> batch_process
-
-    # Dag Structure
-    generate_batches >> batch_processing_group
+    # Task flow. Dynamically create tasks
+    batch_tasks = create_batches(config['batch_size'])
+    batch_results = get_data.expand(batch_task=batch_tasks)
+    cleaned_results = clean_data.expand(batch_json=batch_results)
+    save_to_new_table.expand(cleaned_json=cleaned_results)
